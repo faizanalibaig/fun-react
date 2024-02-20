9905d44877279bef634af292b134786d1bf9463d
faizanalibaig <baig4992@gmail.com>
2024-02-20 22:54:46 +0500

    first upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen bg-slate-300 flex justify-center items-center
+     overflow-x-hidden">
+      <div className="">
+         <h1 className="text-9xl font-kodeMono">'Hello,World'</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
