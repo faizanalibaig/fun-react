e437da5bea6acfd1b0ffab582cf410d6715e28d2
Faizan Ali Baig <142169392+faizanalibaig@users.noreply.github.com>
2024-03-16 23:44:46 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="h-screen w-screen bg-slate-300 flex justify-center items-center
      overflow-x-hidden">
       <div className="">
-         <h1 className="text-9xl font-kodeMono">'Hello,World'</h1>
+         <h1 className="text-9xl font-kodeMono">'Hello,React'</h1>
       </div>
     </div>
   )
